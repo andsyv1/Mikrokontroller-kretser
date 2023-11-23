e563912456b157113faf4a26aa0a70258a9b330e
Kruls <148961331+Kruls@users.noreply.github.com>
2023-11-23 12:02:59 +0100

    Fikset format på kommentarer til GIT

--- SoftBlinkNoDelay.cpp
+++ SoftBlinkNoDelay.cpp
@@ -31,41 +31,41 @@ void pwmInit() {
 	TCCR0A = (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);
 	
 	// De ulike prescaler-verdiene til timer/counter0, og resulterende frekvens.
-	//TCCR0B = (1 << CS00);								 // 0 timer prescaler @ 3.9kHz
-	TCCR0B |= (1 << CS01);								 // 8 timer prescaler @ 488 Hz
-	//TCCR0B |= (1 << CS01) | (1 << CS00);				 // 64 timer prescaler @ 61 Hz
-	//TCCR0B |= (1 << CS02) | (0 << CS01) | (0 << CS00); // 256 timer prescaler @ 15 Hz
-	//TCCR0B |= (1 << CS02) | (0 << CS01) | (1 << CS00); // 1024 timer prescaler @ 4 Hz
+	//TCCR0B = (1 << CS00);					// 0 timer prescaler @ 3.9kHz
+	TCCR0B |= (1 << CS01);					// 8 timer prescaler @ 488 Hz
+	//TCCR0B |= (1 << CS01) | (1 << CS00);			// 64 timer prescaler @ 61 Hz
+	//TCCR0B |= (1 << CS02) | (0 << CS01) | (0 << CS00); 	// 256 timer prescaler @ 15 Hz
+	//TCCR0B |= (1 << CS02) | (0 << CS01) | (1 << CS00); 	// 1024 timer prescaler @ 4 Hz
 	
 	// Startverdi sendt til OCR0A komparator
 	OCR0A = 128;
 	
-	// Skrur på Timer/Counter0 Overflow interrupt
-	// Altså en interrupt varsles når timeren er på topp.
+	// Skrur pÃ¥ Timer/Counter0 Overflow interrupt
+	// AltsÃ¥ en interrupt varsles nÃ¥r timeren er pÃ¥ topp.
 	TIMSK0 |= (1 << TOIE0);
 	
-	// Skrur på globale interrupts
+	// Skrur pÃ¥ globale interrupts
 	sei();
 }
 
-// Dette er funskjonen som kjører for hver gang interrupt blir trigget, som
+// Dette er funskjonen som kjÃ¸rer for hver gang interrupt blir trigget, som
 // skjer ved hver timer0 overflow. 
 ISR(TIMER0_OVF_vect) {
-	// Setter verdi til OCR0A, som betyr PD6 er HØY helt til timer0 == OCR0A 
+	// Setter verdi til OCR0A, som betyr PD6 er HÃ˜Y helt til timer0 == OCR0A 
 	OCR0A = pwmValue;
 
-	// Øker/minker PWM verdien med et inkrement
+	// Ã˜ker/minker PWM verdien med et inkrement
 	pwmValue += increment;
 
-	// Snur retningen på inkrementet, så dioden går fra lysere til mørkere, eller motsatt.
-	// Skjer når PWM har nådd maks eller min oppløsning. 
+	// Snur retningen pÃ¥ inkrementet, sÃ¥ dioden gÃ¥r fra lysere til mÃ¸rkere, eller motsatt.
+	// Skjer nÃ¥r PWM har nÃ¥dd maks eller min opplÃ¸sning. 
 	if (pwmValue == 0 || pwmValue == 255) {
 		increment = -increment;
 	}
 }
 
 int main(void) {
-	// Kun initialiseringen skjer her, da SEI håndterer interrupts internt i microcontrolleren.
+	// Kun initialiseringen skjer her, da SEI hÃ¥ndterer interrupts internt i microcontrolleren.
 	pwmInit();
 
 	while (1) {
